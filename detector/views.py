e2480699524c5d90ac5e85e33b714fb87139ba74
Kamelleon <kamilbugla22@icloud.com>
2022-06-28 19:38:10 +0200

    Created cv2 function to decode image that has been sent.

--- detector/views.py
+++ detector/views.py
@@ -1,5 +1,15 @@
+from django.core.files import File
 from django.shortcuts import render
-
+import cv2
+import numpy as np
 
 def file_uploader_view(request):
+    if request.method == "POST":
+        file = request.FILES["file"]
+        numpy_converted_file = np.fromstring(file.read(), np.uint8)
+        image = cv2.imdecode(numpy_converted_file, cv2.IMREAD_UNCHANGED)
+        if image is None:
+            print("It is not an image")
+        else:
+            print("It is image")
     return render(request, "detector/file_uploader.html")
