d836843fd83fb7a6b247c4b206ed933f2f41cba7
Kamelleon <kamilbugla22@icloud.com>
2022-06-28 17:27:33 +0200

    Created 'file_uploader.html' and attached css to it. Linked file_uploader_view to urls.py

--- CarDetector/urls.py
+++ CarDetector/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from detector.views import file_uploader_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', file_uploader_view),
 ]
--- detector/views.py
+++ detector/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
+def file_uploader_view(request):
+    return render(request, "detector/file_uploader.html")
