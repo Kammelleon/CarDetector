f570f9c649d46b721fee3c53f23c48421493508d
Kamelleon <kamilbugla22@icloud.com>
2022-07-03 17:04:41 +0200

    Changed env settings

--- CarDetector/settings.py
+++ CarDetector/settings.py
@@ -12,23 +12,17 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
-import dotenv
 
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# Load .env file contents
-dotenv_file = os.path.join(BASE_DIR, ".env")
-if os.path.isfile(dotenv_file):
-    dotenv.load_dotenv(dotenv_file)
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY = "django-insecure-vpc#x6lb_zu6c%z$bqf71+b(a^dl6w8j8-n_2a^=e1b8-9)xx2"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
