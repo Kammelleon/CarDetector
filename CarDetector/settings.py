ca2a261cc3675d093e7d7c5929bf1ea9d584ad93
Kamelleon <kamilbugla22@icloud.com>
2022-07-03 21:57:01 +0200

    Changed allowed hosts to all

--- CarDetector/settings.py
+++ CarDetector/settings.py
@@ -12,22 +12,28 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
+import dotenv
 
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# Load .env file contents
+dotenv_file = os.path.join(BASE_DIR, ".env")
+if os.path.isfile(dotenv_file):
+    dotenv.load_dotenv(dotenv_file)
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-vpc#x6lb_zu6c%z$bqf71+b(a^dl6w8j8-n_2a^=e1b8-9)xx2"
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['ancient-lowlands-07767.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -120,7 +126,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
