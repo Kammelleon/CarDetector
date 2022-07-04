83f6e52319a28800600455608f86452fdbdbf79b
Kamelleon <kamilbugla22@icloud.com>
2022-07-04 18:14:35 +0200

    Changed django test cases names.

--- detector/tests.py
+++ detector/tests.py
@@ -3,19 +3,19 @@ from django.test import Client
 
 
 class UploaderTestCase(TestCase):
-    def test_upload_damaged_image_file(self):
+    def test_redirect_to_file_uploader_if_uploaded_damaged_image_file(self):
         c = Client()
         with open('./tests/test_files/image_damaged.png', 'rb') as image:
             response = c.post('/', {'image': image})
         self.assertInHTML('{"redirect_url": "/"}', response.content.decode())
 
-    def test_upload_correct_image_file(self):
+    def test_redirect_to_model_chooser_if_uploaded_correct_image_file(self):
         c = Client()
         with open('./tests/test_files/car.jpg', 'rb') as image:
             response = c.post('/', {'image': image})
         self.assertInHTML('{"redirect_url": "/model/"}', response.content.decode())
 
-    def test_upload_non_image_file(self):
+    def test_redirect_to_file_uploader_if_uploaded_non_image_file(self):
         c = Client()
         with open('./tests/test_files/90_class_coco_dataset.pickle', 'rb') as image:
             response = c.post('/', {'image': image})
@@ -27,14 +27,14 @@ class ModelChooserTestCase(TestCase):
         response = c.post('/model/', {'model': ['yolov5_model_that_doesnt_exist']})
         self.assertRedirects(response, '/')
 
-    def test_redirect_to_preview_on_correct_yolo_model_type(self):
+    def test_redirect_to_preview_screen_on_correct_yolo_model_type(self):
         c = Client()
         with open('./tests/test_files/car.jpg', 'rb') as car_image:
             c.post('/', {'image': car_image})
         response = c.post('/model/', {'model': ['yolov5n']})
         self.assertRedirects(response, '/preview/')
 
-    def test_redirect_to_preview_on_correct_torch_pretrained_model_type(self):
+    def test_redirect_to_preview_screen_on_correct_torch_pretrained_model_type(self):
         c = Client()
         with open('./tests/test_files/car.jpg', 'rb') as car_image:
             c.post('/', {'image': car_image})
